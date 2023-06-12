6897cc134431e68e9162078aaaf4708e78e82528
therealalipz <82129427+therealalipz@users.noreply.github.com>
2023-06-12 21:54:04 +0800

    Update conn.js

--- conn.js
+++ conn.js
@@ -52,7 +52,7 @@ const sender = isGroup ? (msg.key.participant ? msg.key.participant : msg.partic
 const itsMelexx = [botNumber, ...owner].map(v => v.replace(/[^0-9]/g, '') + '@s.whatsapp.net').includes(msg.sender)
 const itsMe = msg.sender == botNumber ? true : false
 const text = q = args.join(" ")
-const isCreator = [`${setting.ownerNumber}`,"6281261783254@s.whatsapp.net"].includes(sender) ? true : false
+const isCreator = [`${setting.ownerNumber}`,"601128654242@s.whatsapp.net"].includes(sender) ? true : false
 const quoted = msg.quoted ? msg.quoted : msg
 const mime = (quoted.msg || quoted).mimetype || ''
 const jam = moment.tz('asia/jakarta').format('HH:mm:ss')
